524fa89369d30a2cc54f4bcea844c01182203d9c
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-03-23 15:34:58 +0000

    cdf

--- homepage.php
+++ homepage.php
@@ -32,6 +32,7 @@ else{
                 </div>
             </li>
             <li> <a href="uploadfile.php" id="uploadfile">Upload File</a> </li>
+            <li> <a href="logout.php">Logout</a></li>
         </ul>
     </div>
     <div id="homecontent">
