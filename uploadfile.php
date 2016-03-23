3e75aa3418fb35a21320fa1acbc1e75a06fc9c30
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-03-23 18:52:07 +0000

    cdf

--- uploadfile.php
+++ uploadfile.php
@@ -25,7 +25,7 @@ session_start();
                     <a href="viewbug2.php">View Bug</a>
                 </div>
             </li>
-            <li> <a href="uploadfile.php" id="uploadfile">Upload File</a> </li>
+            <li> <a href="bug.php" id="uploadfile">Upload File</a> </li>
         </ul>
     </div>
     <div id="homecontent">
