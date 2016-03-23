39ae0652ec0549cf928d3de472a6d837132c4c24
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-03-23 14:55:50 +0000

    cdf

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php
 
-$conn = mysql_connect("localhost", "root", "")
+define('DB_SERVER','eu-cdbr-azure-west-d.cloudapp.net');
+define('DB_USERNAME','bf535786bb30fa');
+define('DB_PASSWORD','88faaa14');
+define('DB_DATABASE','ogunleye');
+
+$conn = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD)
 or die("No database connection");
-mysql_select_db("bugsite", $conn)
+mysql_select_db(DB_DATABASE, $conn)
 or die('No database selected; ' . mysql_error());
 ?>
