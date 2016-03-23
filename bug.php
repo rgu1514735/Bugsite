2f870727edaf3391e82e1c8aed99fcef150bf0ca
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-03-23 17:33:57 +0000

    cdf

--- bug.php
+++ bug.php
@@ -3,7 +3,36 @@ session_start();
 require_once "db.php";
 
 
-    if($_POST['document']) {
+    if($_POST['document'] == '') {
+
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        $postdate = $_POST['postdate'];
+        $userid = $_SESSION['userid'];
+        $username = $_SESSION['username'];
+        $fixeddate = '';
+        $fixed = 0;
+        $bugid = rand(1000,10000);
+
+
+
+        $sql = "insert into bug (title,description,userid,postdate,fixdate,fixed,bugid,username) values ('$title','$description','$userid','$postdate','$fixeddate','$fixed','$bugid','$username')";
+        if($result = mysql_query($sql)) {
+            echo '<script type="text/javascript">';
+            echo 'alert("Bug is posted");';
+            echo 'window.location.href = "homepage.php";';
+            echo '</script>';
+        }
+        else
+        {
+            echo "there is an non file error";
+        }
+
+
+
+    }
+   else
+    {
         $title = $_POST['title'];
         $description = $_POST['description'];
         $postdate = $_POST['postdate'];
@@ -31,58 +60,33 @@ require_once "db.php";
             echo $ext;
 
 
-           /* if ($ext == "txt" || $ext == "JPG" || $ext == "PNG" || $ext == "jpg" || $ext == "png") {
-                //$filename = $bugid;
-                move_uploaded_file($_FILES['document']['tmp_name'], $upload_dir . '/' . $filename);
-                $sql = "insert into bug (title,description,userid,postdate,fixdate,fixed,bugid,username) values ('$title','$description','$userid','$postdate','$fixeddate','$fixed','$bugid','$username')";
+            /* if ($ext == "txt" || $ext == "JPG" || $ext == "PNG" || $ext == "jpg" || $ext == "png") {
+                 //$filename = $bugid;
+                 move_uploaded_file($_FILES['document']['tmp_name'], $upload_dir . '/' . $filename);
+                 $sql = "insert into bug (title,description,userid,postdate,fixdate,fixed,bugid,username) values ('$title','$description','$userid','$postdate','$fixeddate','$fixed','$bugid','$username')";
 
-                if ($result = mysql_query($sql)) {
+                 if ($result = mysql_query($sql)) {
 
-                    $url = $upload_dir . '/' . $filename;
+                     $url = $upload_dir . '/' . $filename;
 
-                    $sql2 = "insert into attachment (attachmentid,url,bugid,userid) values ('$attachmentid','$url','$bugid','$userid')";
-                    echo '<script type="text/javascript">';
-                    echo 'alert("Bug is posted");';
-                    echo 'window.location.href = "homepage.php";';
-                    echo '</script>';
-                } else {
-                    echo "there is an error in file portion";
-                }
+                     $sql2 = "insert into attachment (attachmentid,url,bugid,userid) values ('$attachmentid','$url','$bugid','$userid')";
+                     echo '<script type="text/javascript">';
+                     echo 'alert("Bug is posted");';
+                     echo 'window.location.href = "homepage.php";';
+                     echo '</script>';
+                 } else {
+                     echo "there is an error in file portion";
+                 }
 
-            } else {
-                /*echo '<script type="text/javascript">';
-                echo 'alert("Invalid file type. Only text file accepted");';
-                echo 'window.location.href = "homepage.php";';
-                echo '</script>';
-                echo 'failed';
+             } else {
+                 /*echo '<script type="text/javascript">';
+                 echo 'alert("Invalid file type. Only text file accepted");';
+                 echo 'window.location.href = "homepage.php";';
+                 echo '</script>';
+                 echo 'failed';
 
-            }*/
+             }*/
 
-        }
-    }
-   else
-    {
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-        $postdate = $_POST['postdate'];
-        $userid = $_SESSION['userid'];
-        $username = $_SESSION['username'];
-        $fixeddate = '';
-        $fixed = 0;
-        $bugid = rand(1000,10000);
-
-
-
-        $sql = "insert into bug (title,description,userid,postdate,fixdate,fixed,bugid,username) values ('$title','$description','$userid','$postdate','$fixeddate','$fixed','$bugid','$username')";
-        if($result = mysql_query($sql)) {
-            echo '<script type="text/javascript">';
-            echo 'alert("Bug is posted");';
-            echo 'window.location.href = "homepage.php";';
-            echo '</script>';
-        }
-        else
-        {
-            echo "there is an non file error";
         }
      }
 
