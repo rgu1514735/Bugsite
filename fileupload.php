5075677d467f6fcbf9073c44f30b8678e28c9663
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-03-23 19:32:04 +0000

    cdf

--- fileupload.php
+++ fileupload.php
@@ -23,24 +23,36 @@ $upload_dir = "uploads";
 
         $filename = $_FILES['document']['name'];
             $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+
+        if ($ext == "txt" || $ext == "JPG" || $ext == "PNG" || $ext == "jpg" || $ext == "png") {
             echo $ext;
+            //$filename = $bugid;
+            /*move_uploaded_file($_FILES['document']['tmp_name'], $upload_dir . '/' . $filename);
+            $sql = "insert into bug (title,description,userid,postdate,fixdate,fixed,bugid,username) values ('$title','$description','$userid','$postdate','$fixeddate','$fixed','$bugid','$username')";
 
-            /*if ($ext == "txt" || $ext == "JPG" || $ext == "PNG"|| $ext == "jpg"|| $ext == "png") {
-                move_uploaded_file($_FILES['document']['tmp_name'], $upload_dir . '/' . $filename);
-                //echo $filename;
+            if ($result = mysql_query($sql)) {
+
+                $url = $upload_dir . '/' . $filename;
+
+                $sql2 = "insert into attachment (attachmentid,url,bugid,userid) values ('$attachmentid','$url','$bugid','$userid')";
                 echo '<script type="text/javascript">';
-                echo 'alert("File Uploaded");';
-                echo 'window.location.href = "uploadfile.php";';
+                echo 'alert("Bug is posted");';
+                echo 'window.location.href = "homepage.php";';
                 echo '</script>';
-
             } else {
-                echo '<script type="text/javascript">';
-                echo 'alert("Invalid file type. Only text file accepted");';
-                echo 'window.location.href = "uploadfile.php";';
-                echo '</script>';
-
+                echo "there is an error in file portion";
             }*/
 
+        } else {
+            echo '<script type="text/javascript">';
+            echo 'alert("Invalid file type. Only text file accepted");';
+            echo 'window.location.href = "homepage.php";';
+            echo '</script>';
+            echo 'failed';
+
+
+        }
     }
 
 ?>
