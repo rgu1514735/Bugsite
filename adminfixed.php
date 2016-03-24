c225da6e0bcd26b14e54876f3e0e7ac4cbaf2f8f
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-03-24 18:28:15 +0000

    cdf

--- adminfixed.php
+++ adminfixed.php
@@ -9,31 +9,24 @@ $commentid = rand(100000,999999);
 $postdate = date('d/m/y');
 $fixed = $_POST['fix'];
 
-if($_POST['fix'] == 'fixed')
-{
-    echo 'fixed';
-}
-else {
-    echo 'not fixed';
-}
 
 
 
 
-/*$sql = "insert into comment_table (comment,userid,postdate,bugid,commentid,username) values ('$usercomment','$userid','$postdate','$bugid','$commentid','$username')";
+
+$sql = "insert into comment_table (comment,userid,postdate,bugid,commentid,username) values ('$usercomment','$userid','$postdate','$bugid','$commentid','$username')";
 if($result = mysqli_query($sql))
 {
-    if($fixed = 'fixed')
+    if($_POST['fix'] == 'fixed')
     {
-        echo $fixed;
+        echo 'fixed';
     }
-    else
-    {
-        echo $fixed;
+    else {
+        echo 'not fixed';
     }
 echo '<script type="text/javascript">';
     echo 'alert("comment posted");';
-    echo 'window.location.href = "viewbug2.php";';
+    echo 'window.location.href = "admminviewbug.php";';
     echo '</script>';
 }
 else
@@ -42,6 +35,6 @@ echo '<script type="text/javascript">';
     echo 'alert("i didnt work o");';
     echo 'window.location.href = "viewbug2.php";';
     echo '</script>';
-}*/
+}
 
 ?>
