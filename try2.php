84cc453e12f3a9c75d91884147983d5bb9b48b68
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-03-24 18:02:54 +0000

    cdf

--- try2.php
+++ try2.php
@@ -2,7 +2,10 @@
                     <form method="post" action="try.php">
 
 
-                           <select name="fix"> <option value="fixed"></option> <option value="not fixed"></option></select>
+                           <select name="fix">
+                               <option value="fixed"> Fixed</option>
+                               <option value="not fixed">Not fixed</option>
+                           </select>
                             <input type="submit" value="Post"/> <input type="reset" value="Cancel" />
 
 
