23678328add476fccac814dd68a4a9a138335269
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-03-24 13:05:35 +0000

    cdf

--- try.php
+++ try.php
@@ -1,47 +1,26 @@
 <?php
 
-/*require_once "db.php";
-
-$bugnumber = "select * from bug ";
-if($res = mysql_query($bugnumber))
-{
-
-    $size_of_array = mysql_num_rows($res);
-    echo $size_of_array;
-    while($result = mysql_fetch_assoc($res))
-    {
-        echo "   ";
-        echo $result['title'];
+require_once "db.php";
 
 
-        echo "   ";
-        echo $result['description'];
+$userid = 'userid';
 
-        echo $result['userid'];
-        echo "   ";
-        echo $result['postdate'];
-        echo "<br/>";
-
+$fixeddate = '';
+$fixed = 0;
+$bugid = rand(1000, 10000);
+$attachmentid = rand(1000,10000);
 
 
+$url = '';//$upload_dir . '/' . $filename;
 
-
-
-    }
-
-}*/
-$i=0;
-    while($i<10) {
-        $j = $i;
-        echo "<form method='post' action='try2.php'>";
-        echo "<input type='text' value='$i' name='$j'/>";
-        echo "<input type='submit' name='$i'/>";
-        $_SESSION['i'] = $i;
-        echo "</form>";
-        $i++;
-
-    }
-
+$sql2 = "insert into attachment(attachmentid,url,bugid,userid) values ('$attachmentid','$url','$bugid','$userid')";
+ if(mysql_query($sql2))
+ {
+     echo 'worked';
+ }
+else{
+    echo 'not worked';
+}
 
 
 
